9761e951ae927ff68414835d813a56132a04cdce
7cggshdvyn-art <7cggshdvyn@privaterelay.appleid.com>
2026-01-16 14:16:06 +0800

    finish

--- js/data/weapon/magnan/data_magnan.js
+++ js/data/weapon/magnan/data_magnan.js
@@ -40,6 +40,47 @@ const magnan = [
       }
     }
   },
+  {
+    id: "EagalOfDesert",
+    name: "沙漠之鹰",
+
+    base: {
+      gameId: 785,
+      price: 8620,
+      weight: 2,        
+      quality: 4,
+      tags: ["枪械", "武器", "马格南", "可维修"],
+
+      description: "虽然是手枪的尺寸用的也是手枪的配件，但使用大威力的马格南弹药。"
+    },
+
+    stats: {
+      caliber: "MAG",          
+      durability: 100,        
+      damage: 28.5,            
+      fireRate: 2.5,  
+      magSize: 7,        
+      reloadTime: 3,          
+      bulletSpeed: 93,       
+      range: 25.2,              
+
+      critMultiplier: 1.6,  
+      soundRange: 32.4,       
+      aimTime: 0.65,          
+
+      moveSpeedCoef: 0.92,    
+      aimSpeedCoef: 0.55,
+      explosiveDamageCoef: 1, 
+
+      hipSpread: 16.6,       
+      aimSpread: 10.14,       
+
+      recoil: {
+        vertical: 113.5,      
+        horizontal: 94        
+      }
+    }
+  },
 ];
 
 export default magnan;
