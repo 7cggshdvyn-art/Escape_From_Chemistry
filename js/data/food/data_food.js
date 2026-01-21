574bce8016b775057ac5c2b47f50ae3affdbb1df
karsonho-svg <karsonho04@gmail.com>
2026-01-21 21:22:32 +0800

    加入食物

---
+++ js/data/food/data_food.js
@@ -0,0 +1,22 @@
+const food = [
+  {
+    id: "cola",
+    name: "可乐",
+
+    base: {
+      gameId: 14,
+      price: 1995,
+      weight: 0.36,        
+      quality: 4,
+      tags: ["食物", "饮品"],
+
+      description: "有糖的可乐才有灵魂。"
+    },
+
+    stats: {
+      hpIncrease: 60,
+    }
+  },
+];
+
+export default food;
